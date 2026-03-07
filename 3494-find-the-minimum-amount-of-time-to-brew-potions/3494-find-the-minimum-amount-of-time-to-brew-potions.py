b9553bf018a8d79b3358834849fd140cebfdc7b0
Abhranil Nanda <147373115+jusst-ronnie@users.noreply.github.com>
2026-03-07 10:45:59 +0530

    Time: 9553 ms (69.76%), Space: 20.2 MB (65.64%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
@@ -3,19 +3,23 @@ class Solution:
         n = len(skill)
         m = len(mana)
 
+        # prefix sums of skill
         prefix = [0]*n
         prefix[0] = skill[0]
         for i in range(1,n):
             prefix[i] = prefix[i-1] + skill[i]
 
-        start = 0
+        finish = [0]*n
 
-        for j in range(1, m):
-            prev = start
-            need = prev
+        for j in range(m):
+            start = finish[0]
+
+            for i in range(1,n):
+                start = max(start, finish[i] - mana[j]*prefix[i-1])
+
+            cur = start
             for i in range(n):
-                prev_prefix = prefix[i-1] if i > 0 else 0
-                need = max(need, prev + mana[j-1]*prefix[i] - mana[j]*prev_prefix)
-            start = need
+                cur += skill[i]*mana[j]
+                finish[i] = cur
 
-        return start + mana[-1]*prefix[-1]
+        return finish[-1]
