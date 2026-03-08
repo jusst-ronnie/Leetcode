3f7f7fac7387e60325a3f94483464afc48be2c03
Abhranil Nanda <147373115+jusst-ronnie@users.noreply.github.com>
2026-03-08 10:12:41 +0530

    Time: 372 ms (86.09%), Space: 42.3 MB (50.94%) - LeetHub

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
@@ -1,26 +1,26 @@
+from collections import Counter
+
 class Solution:
     def maximumTotalDamage(self, power):
-        from collections import Counter
-        
         count = Counter(power)
-        values = sorted(count.keys())
-        n = len(values)
-
-        total = [v * count[v] for v in values]
-
+        nums = sorted(count.keys())
+        
+        values = [x * count[x] for x in nums]
+        n = len(nums)
+        
         dp = [0] * n
-
+        
         for i in range(n):
-            take = total[i]
-
+            take = values[i]
+            
             j = i - 1
-            while j >= 0 and values[i] - values[j] <= 2:
+            while j >= 0 and nums[i] - nums[j] <= 2:
                 j -= 1
-
+            
             if j >= 0:
                 take += dp[j]
-
+            
             skip = dp[i-1] if i > 0 else 0
-            dp[i] = max(skip, take)
-
+            dp[i] = max(take, skip)
+        
         return dp[-1]
