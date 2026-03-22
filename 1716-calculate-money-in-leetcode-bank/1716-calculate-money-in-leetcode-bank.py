b30d79b59b9f2f85d983d90a2149261805218081
Abhranil Nanda <147373115+jusst-ronnie@users.noreply.github.com>
2026-03-22 19:44:41 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.13%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,12 +1,15 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        weeks = n // 7
-        days = n % 7
+        total = 0
+        monday_value = 1
         
-        total = 28 * weeks + 7 * (weeks * (weeks - 1) // 2)
-        
-        start = weeks + 1
-        for i in range(days):
-            total += start + i
+        # Iterate through n days
+        for i in range(n):
+            # i // 7 gives the week offset (0, 1, 2...)
+            # i % 7 gives the day of the week offset (0, 1, 2, 3, 4, 5, 6)
+            week_offset = i // 7
+            day_offset = i % 7
+            
+            total += (monday_value + week_offset + day_offset)
             
         return total
